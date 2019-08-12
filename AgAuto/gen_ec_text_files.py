02e185850244d101ceb73c8a2c742df93875141d
MBagServer <C.Amao@gov.mb.ca>
2019-08-12 13:47:47 -0500

    run-all plus comments

--- AgAuto/gen_ec_text_files.py
+++ AgAuto/gen_ec_text_files.py
@@ -22,9 +22,9 @@ def main():
         with open(get_path_dir('output_files', file_name_60), 'w+') as hourly_file:
             hourly_file.write(parse.gen_string_rep(hourly_data_array.get_data(each_id)))
 
-        batch_file.insert_command('copy %s C:\\wamp\\www\\ec_files' % file_name_60)
+        batch_file.insert_command('copy %s C:\\WWW\\live.mbagweather.ca\\ec_files' % file_name_60)
         batch_file.insert_command('del %s' % file_name_60)
-        batch_file.insert_command('copy %s C:\\wamp\\www\\ec_files' % file_name_24)
+        batch_file.insert_command('copy %s C:\\WWW\\live.mbagweather.ca\\ec_files' % file_name_24)
         batch_file.insert_command('del %s' % file_name_24)
 
     batch_file.export('move_ec_texts.bat')
